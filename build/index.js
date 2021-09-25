1f05ea423d99c60951909050362a12bbae7535ac
Angie Garcia <anyigarcia07@gmail.com>
2021-09-24 19:14:01 -0500

    servidor

--- build/index.js
+++ build/index.js
@@ -1,2 +1,24 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+class Server {
+    constructor() {
+        this.app = express_1.default();
+        this.config();
+        this.route();
+    }
+    config() {
+        this.app.set('Port', process.env.PORT || 3000); //VERIFICAMOS QUE EL PUERTO ESTE LIBRE 
+    }
+    route() {
+    }
+    star() {
+        this.app.listen(this.app.get('Port'));
+        console.log(`Server on port ${this.app.get('Port')}`);
+    } //inicializar servidor
+}
+const server = new Server();
+server.star();
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
 //console.log('Esto funciona')
-import express from 'express'
+import indexRoute from './routes/indexRoutes';
+import express, {Application} from 'express'
+
+
+class Server { //Clase que iniciará el servidor
+    public app:Application;
+    constructor(){ //constructor inicializa 
+        this.app = express();
+        this.config();
+        this.route();
+
+    }
+
+    config():void { //configura la propiedad app
+        this.app.set('Port' , process.env.PORT || 3000); //VERIFICAMOS QUE EL PUERTO ESTE LIBRE 
+    }
+
+    route():void { //va a configurar las rutas de App
+
+
+    }
+    star():void{
+        this.app.listen(this.app.get('Port'))
+        console.log( `Server on port ${this.app.get('Port')}` )
+    }//inicializar servidor
+
+
+
+}
+
+const server = new Server();
+
+server.star()
