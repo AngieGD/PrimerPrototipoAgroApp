16423481ea528c3e50c8a6d0c599958d701a37ef
Angie Garcia <anyigarcia07@gmail.com>
2021-10-03 12:16:24 -0500

    Con Api

--- server/build/controllers/gamesControlers.js
+++ server/build/controllers/gamesControlers.js
@@ -1,27 +1,68 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+//import {createPool} from 'promise-mysql';
+//import keys from '../keys';
+//const connection = createPool(keys.database);
+const database_1 = __importDefault(require("../database"));
 // traer conexión 
 //const connect  = require('../database');
 class IndexgameControler {
     list(req, res) {
-        // connection.query('DESCRIBE games');
-        //pool.query('DESCRIBE games')
-        res.json('listing games');
+        return __awaiter(this, void 0, void 0, function* () {
+            // connection.query('DESCRIBE games');
+            const games = yield database_1.default.query('SELECT * FROM games');
+            res.json(games);
+            //pool.query('DESCRIBE games')
+            //res.json('listing games')
+        });
     }
     oneGame(req, res) {
-        res.json({ text: `One game ${req.params.id}` });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            const games = yield database_1.default.query('SELECT * FROM games WHERE id = ? ', [id]);
+            if (games.length > 0) {
+                return res.json(games[0]);
+            }
+            res.status(404).json({ text: "The game doesn't exist " });
+            console.log(games);
+        });
     }
     //Metodo para crear un juego
     create(req, res) {
-        console.log(req.body); //Cuando angular envie datos va a ser por medio de este req.body
-        res.json({ text: 'creating a game' });
-        //Crear consulta
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('INSERT INTO games set ?', [req.body]);
+            //console.log(req.body); //Cuando angular envie datos va a ser por medio de este req.body
+            res.json({ message: 'game saved' });
+            //Crear consulta
+        });
     }
     delete(req, res) {
-        res.json({ text: `deleting a game ${req.params.id}` });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            database_1.default.query('DELETE FROM games WHERE id = ?', [id]);
+            yield res.json({ text: `deleting a game ${req.params.id}` });
+            res.json({ message: 'The game was deleted' });
+        });
     }
     update(req, res) {
-        res.json({ text: `update a game ${req.params.id}` });
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = req.params;
+            yield database_1.default.query('UPDATE games set ? WHERE id = ? ', [req.body, id]);
+            //res.json({text:`update a game ${req.params.id}`})
+            res.json({ message: 'The Game was Update' });
+        });
     }
 }
 ;
--- server/src/controllers/gamesControlers.ts
+++ server/src/controllers/gamesControlers.ts
@@ -8,30 +8,46 @@ import {Request , Response } from 'express';
    
 //const connect  = require('../database');
 class IndexgameControler {
-    public list (req:Request , res:Response) {
+    public async list (req:Request , res:Response) {
        // connection.query('DESCRIBE games');
-
+        const games = await pool.query('SELECT * FROM games')
+        res.json(games);
         //pool.query('DESCRIBE games')
-        res.json('listing games')
+        //res.json('listing games')
     }
 
-    public oneGame(req:Request , res:Response){
-        res.json({text: `One game ${req.params.id}`})
+    public async oneGame(req:Request , res:Response): Promise<any>{
+        const {id} = req.params;
+        const games = await pool.query('SELECT * FROM games WHERE id = ? ' , [id]);
+        if(games.length > 0){
+            return res.json(games[0])
+        }
+        res.status(404).json({text: "The game doesn't exist "});
+        console.log(games);
     }
 
     //Metodo para crear un juego
-    public create (req:Request , res:Response){
-        console.log(req.body); //Cuando angular envie datos va a ser por medio de este req.body
-        res.json({text: 'creating a game'});
+    public async create (req:Request , res:Response){  //Las consultas a la base de datos son eventos asincronos, por eso se pone async
+        await pool.query('INSERT INTO games set ?' , [req.body]);
+        //console.log(req.body); //Cuando angular envie datos va a ser por medio de este req.body
+        res.json({message: 'game saved'});
         //Crear consulta
     }
 
-    public delete (req:Request , res:Response){
-        res.json({text: `deleting a game ${req.params.id}`});
+    public async delete (req:Request , res:Response) : Promise<void>{
+        const {id} = req.params
+        pool.query('DELETE FROM games WHERE id = ?' , [id]);
+        await res.json({text: `deleting a game ${req.params.id}`});
+        res.json({message : 'The game was deleted'});
+
     }
 
-    public update (req:Request , res:Response){
-        res.json({text:`update a game ${req.params.id}`})
+    public async update (req:Request , res:Response): Promise<void>{
+        const{id} = req.params;
+        await pool.query('UPDATE games set ? WHERE id = ? ' , [req.body , id]);
+        //res.json({text:`update a game ${req.params.id}`})
+        res.json({message: 'The Game was Update'});
+
     }
 };
 
