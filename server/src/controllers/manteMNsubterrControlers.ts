f6d630ebfad06f015fff566b631fa36162c5c1fb
Angie Garcia <anyigarcia07@gmail.com>
2021-10-04 17:15:12 -0500

    Quuite algo

---
+++ server/src/controllers/manteMNsubterrControlers.ts
@@ -0,0 +1,9 @@
+import pool from "../database";
+import {Request , response, Response } from 'express';
+import mantenimientoroutes from '../routes/mantenimientosroutes';
+
+class MantenimientoMNsubteControlers {
+    
+}
+
+const manteniMNsubt = new MantenimientoMNsubteControlers();
